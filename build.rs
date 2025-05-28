fe244395c37ceebe23df368f73df6005ca65b5ab
Steven Allen <steven@stebalien.com>
2025-05-28 15:35:03 -0700

    fix build script and update gazetta

--- build.rs
+++ build.rs
@@ -3,16 +3,19 @@ use std::{env, io};
 use clap::{CommandFactory, ValueEnum};
 
 fn main() -> Result<(), io::Error> {
-    let outdir = match env::var_os("OUT_DIR") {
-        None => return Ok(()),
-        Some(outdir) => outdir,
+    let Some(outdir) = env::var_os("OUT_DIR") else {
+        return Ok(());
     };
 
     let mut cmd = gazetta::cli::Cli::command();
-    let name = cmd.get_name().to_string();
+    let Some(name) = env::var_os("CARGO_BIN_NAME") else {
+        // Not building the binary.
+        return Ok(());
+    };
 
+    let name = name.to_string_lossy();
     for &shell in clap_complete::Shell::value_variants() {
-        clap_complete::generate_to(shell, &mut cmd, &name, &outdir)?;
+        clap_complete::generate_to(shell, &mut cmd, &*name, &outdir)?;
     }
     Ok(())
 }
